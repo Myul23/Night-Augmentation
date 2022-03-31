e30d8d58e4e677fc1891c1efe570fb1689312cc0
Myul23 <Myul23@users.noreply.github.com>
2022-03-31 16:38:27 +0900

    fighting gpus

--- Technique check/style_select_transfer.py
+++ Technique check/style_select_transfer.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 
 
-data_path = "C:/making_origin"
-save_path = "augmentation"
+data_path = "images"
+save_path = "models"
 
 if not os.path.exists(data_path):
     print("No data folder")
@@ -33,31 +33,42 @@ for folder in os.listdir(data_path):
         result2 = os.path.join(style2, img_file)
 
         img = cv2.imread(img_path)
-        cv2.imwrite(result1, img)
+        # cv2.imwrite(result1, img)
 
         # TODO 만든 데이터 쪽
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        # h, s, v = cv2.split(img)
+        base = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(base)
 
-        # dst = np.array(v)
-        # dst = np.exp((dst - np.mean(dst)) / np.std(dst))
-        # dst = dst * 150 / np.max(dst.flatten())
+        dst = np.array(v)
+        dst = np.exp((dst - np.mean(dst)) / np.std(dst))
+        dst = dst * 100 / np.max(dst.flatten())  # 150
 
-        # img = cv2.merge((h, s, dst.astype(np.uint8)))
-        # img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-        # cv2.imwrite(result2, img)
+        img1 = cv2.merge((h, s, dst.astype(np.uint8)))
+        img1 = cv2.cvtColor(img1, cv2.COLOR_HSV2BGR)
 
         # TODO crawling 쪽
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(img)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # h, s, v = cv2.split(img)
 
         dst = np.add(255 - v, 56).astype(np.uint8)
         dst = cv2.subtract(dst, 56)
         dst = np.exp((dst - np.mean(dst)) / np.std(dst))
-        dst = dst * 150 / np.max(dst.flatten())
+        dst = dst * 100 / np.max(dst.flatten())  # 150
         dst = np.add(dst, 20)
 
-        img = cv2.merge((h, s, dst.astype(np.uint8)))
-        img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-        cv2.imwrite(result2, img)
+        img2 = cv2.merge((h, s, dst.astype(np.uint8)))
+        img2 = cv2.cvtColor(img2, cv2.COLOR_HSV2BGR)
+
+        cv2.imshow("origin", img)
+        cv2.imshow("A_making", img1)
+        cv2.imshow("D_crawling", img2)
+        temp = cv2.waitKey()
+        if temp == ord("a"):
+            os.remove(img_path)
+            cv2.imwrite(result2, img1)
+        elif temp == ord("d"):
+            os.remove(img_path)
+            cv2.imwrite(result2, img2)
+        else:
+            cv2.imwrite(os.path.join(os.path.join("images", folder), img_file), img)
     print(folder, "finished")
